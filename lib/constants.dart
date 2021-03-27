31a4cac168b5de7019440605d88a4a7d9c8850fd
mrcodefrost <72447688+mrcodefrost@users.noreply.github.com>
2021-03-27 14:05:16 +0530

    updated UI

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kTextLightColor = Color(0xFF959595);
+const kTitleTextColor = Color(0xFF303030);
+
+const kHeadingTextStyle = TextStyle(
+  fontFamily: 'Quicksand',
+  fontSize: 22,
+  fontWeight: FontWeight.w900,
+);
+
+const kTitleTextstyle = TextStyle(
+  fontFamily: 'Quicksand',
+  fontSize: 18,
+  color: kTitleTextColor,
+  fontWeight: FontWeight.bold,
+);
+const kSubTextStyle =
+    TextStyle(fontFamily: 'Amatic SC', fontSize: 16, color: kTextLightColor);
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,6 @@
-import 'package:bitbox_220_volts/models/sensor_data.dart';
+import 'package:bitbox_220_volts/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomeScreen extends StatefulWidget {
   @override
@@ -11,38 +12,74 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('RAHAT'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(15.0),
-                child: Text(
-                  'Hello',
-                  style: TextStyle(
-                    color: Colors.pinkAccent,
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [Color(0xFFE91E63), Color(0xFFFF80AB)]),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                        child: Stack(
+                      children: <Widget>[
+                        Container(),
+                        SvgPicture.asset(
+                          "assets/nurse.svg",
+                          width: 225,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.bottomLeft,
+                        ),
+                        Positioned(
+                          top: 70,
+                          left: 180,
+                          child: Text(
+                            'Remote Application \n For Health \n Assistance And \n Tracking',
+                            style: kHeadingTextStyle.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))
+                  ],
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(15.0),
-                color: Colors.purpleAccent.shade100,
-                child: FlatButton(
-                  child: Text('Tap Me'),
-                  onPressed: () {
-                    setState(() {
-                      SensorData().getSensorData();
-                    });
-                  },
-                ),
-              )
-            ],
-          )
-        ],
+            ),
+            Container(
+              color: Colors.black,
+              height: 20.0,
+              width: 20.0,
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
